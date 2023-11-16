0be4384ef70b807a52596823d38c8fc6f66ca390
Arne Hanssen <arne.hanssen@nav.no>
2023-11-16 14:37:29 +0100

    Refactor environmental variables
    Refactor fetching of environmental variables to use system wrapper.
    This in order to facilitate unit testing.

--- src/main/kotlin/no/nav/sf/brukernotifikasjon/service/BrukernotifikasjonService.kt
+++ src/main/kotlin/no/nav/sf/brukernotifikasjon/service/BrukernotifikasjonService.kt
@@ -7,18 +7,19 @@ import no.nav.brukernotifikasjon.schemas.input.InnboksInput
 import no.nav.brukernotifikasjon.schemas.input.NokkelInput
 import no.nav.sf.brukernotifikasjon.config.Environment
 import no.nav.sf.brukernotifikasjon.config.KafkaConfig
+import no.nav.sf.brukernotifikasjon.config.StringEnvVar.getEnvVar
 import org.apache.kafka.clients.producer.KafkaProducer
 
 class BrukernotifikasjonService {
     val environment = Environment()
     val kafkaProducerDone = KafkaProducerWrapper(
-        System.getenv("KAFKA_TOPIC_DONE"),
+        getEnvVar("KAFKA_TOPIC_DONE"),
         KafkaProducer<NokkelInput, DoneInput>(
             KafkaConfig.producerProps(environment, Eventtype.DONE)
         )
     )
     val kafkaProducerInnboks = KafkaProducerWrapper(
-        System.getenv("KAFKA_TOPIC_INNBOKS"),
+        getEnvVar("KAFKA_TOPIC_INNBOKS"),
         KafkaProducer<NokkelInput, InnboksInput>(
             KafkaConfig.producerProps(environment, Eventtype.INNBOKS)
         )
